6c1e50dd03fde1f4c43723bbe7ef7fc82e6faf9e
Stephen Liedig <sliedig@amazon.com>
2019-04-26 14:50:47 +0800

    Package and tooling updates

--- hooks/post_gen_project.py
+++ hooks/post_gen_project.py
@@ -1,29 +1,20 @@
-"""
-    Given that this cookiecutter will run to scaffold a Python
-    Lambda project and the user may not want to use the Makefile
-    so we will remove it if that's the case.
-"""
-
 from __future__ import print_function
-
-import os
 
 TERMINATOR = "\x1b[0m"
 INFO = "\x1b[1;33m [INFO]: "
 SUCCESS = "\x1b[1;32m [SUCCESS]: "
 HINT = "\x1b[3;33m"
 
-def main():
 
+def main():
     solution_name = '{{ cookiecutter.solution_name }}'
-    project_name = '{{ cookiecutter.project_name }}'
 
     print(SUCCESS +
           "Project initialized successfully! You can now jump to {} folder".
-          format(project_name) + TERMINATOR)
+          format(solution_name) + TERMINATOR)
     print(INFO +
           "{}/README.md contains instructions on how to proceed.".
-          format(project_name) + TERMINATOR)
+          format(solution_name) + TERMINATOR)
 
 
 if __name__ == '__main__':
